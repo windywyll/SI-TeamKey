9eeebea512f8dd61b53fcbca1269956378ef3c8b
Windywyll <appleteensee@yahoo.fr>
2016-05-09 18:22:44 +0200

    Bullet movement

---
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour {
+
+    public float m_speed = 40f;
+    public float m_damage;
+    public GameObject m_creator;
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, 12);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + transform.forward * Time.fixedDeltaTime * m_speed;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(gameObject);
+    }
+}
