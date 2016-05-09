79feec6d2057fb2f9f6f27d40f28b561d0732f60
Windywyll <appleteensee@yahoo.fr>
2016-05-09 18:29:25 +0200

    self destruct

--- Assets/Scripts/Bullet/BulletMovement.cs
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class BulletMovement : MonoBehaviour {
 
-    public float m_speed = 40f;
-    public float m_damage;
-    public GameObject m_creator;
+    [SerializeField]
+    private float m_lifeSpan = 12.0f;
+    [SerializeField]
+    private float m_speed = 40f;
+
+    private float m_damage;
+    private float startDeathCountdown;
+    private GameObject m_creator;
+
     // Use this for initialization
     void Start()
     {
         Destroy(gameObject, 12);
+        startDeathCountdown = Time.time;
+    }
+
+    void Update()
+    {
+        if (startDeathCountdown + m_lifeSpan < Time.time)
+            Destroy(gameObject);
     }
 
     void FixedUpdate()
