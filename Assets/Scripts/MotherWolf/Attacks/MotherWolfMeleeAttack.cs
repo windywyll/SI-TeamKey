98270811e44f56d5285929fd1f065fee8b3c3ef0
Windywyll <appleteensee@yahoo.fr>
2016-05-10 10:58:07 +0200

    In the year of the wolf

---
+++ Assets/Scripts/MotherWolf/Attacks/MotherWolfMeleeAttack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotherWolfMeleeAttack : MotherWolfAttack {
+
+	// Use this for initialization
+	void Start () {
+        m_damage = 10.0f;
+        m_percentageAttack = 100.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setTarget(GameObject target)
+    {
+        m_target = target;
+    }
+
+    public override void launchAttack()
+    {
+        Debug.Log("Attack!");
+    }
+}
---
+++ Assets/Scripts/MotherWolf/MotherWolfMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotherWolfMovement : MonoBehaviour {
+
+    //[SerializeField]
+    //private float m_rotatingSpeed;
+    
+    private Vector3 m_initPos;
+    private Vector3 m_target;
+
+	// Use this for initialization
+	void Start () {
+        m_initPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(m_target != null)
+        {
+            m_target.y = m_initPos.y;
+            transform.LookAt(m_target);
+        }
+	}
+}
