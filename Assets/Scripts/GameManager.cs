3414dfea22b71f4ac03806b0123a415e2e6fdd12
Baptiste <mail@baptistebillet.com>
2016-05-11 21:19:33 +0200

    Add player array

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class GameManager : MonoBehaviour {
 
     public int m_PlayerNumber;
 
     public GameObject m_Player;
+
+    public Player[] m_PlayerArray = new Player[4];
 
 	// Use this for initialization
 	void Awake ()
@@ -28,16 +32,18 @@ public class GameManager : MonoBehaviour {
         {
             for(int i=m_PlayerNumber; i>0; i--)
             {
-                PlayerInstantiate(i);
+                
+                m_PlayerArray[i-1]= PlayerInstantiate(i);
             }
         }
     }
 
-    void PlayerInstantiate(int _id)
+    Player PlayerInstantiate(int _id)
     {
         GameObject _go = Instantiate(m_Player, Vector3.zero, Quaternion.identity) as GameObject;
         _go.GetComponent<Player>().m_PlayerId = _id;
         GetComponent<DataSorting>().AddData(_go.GetComponent<PlayerData>());
+        return _go.GetComponent<Player>();
     }
 
 }
